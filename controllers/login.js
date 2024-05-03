0fbfd12755838c536d62eecebf65da601416d821
Parsakaali <a@a.com>
2024-05-03 23:04:10 +0300

    13.24 underway - sessions table, add to sessions on login

--- controllers/login.js
+++ controllers/login.js
@@ -4,34 +4,48 @@ const router = require('express').Router()
 const { SECRET } = require('../util/config')
 
 const User = require('../models/user')
+const Session = require('../models/session')
 
 router.post('/', async (request, response) => {
-  const body = request.body
+  try{
+    const body = request.body
 
-  const user = await User.findOne({
-    where: {
-      username: body.username
-    }
-  })
-
-  const passwordCorrect = body.password === 'password'
-
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: 'invalid username or password'
+    const user = await User.findOne({
+      where: {
+        username: body.username
+      }
     })
+
+    const passwordCorrect = body.password === 'password'
+
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: 'invalid username or password'
+      })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user.id,
+    }
+
+    const token = jwt.sign(userForToken, SECRET)
+
+    console.log("Session", userForToken.id, token)
+
+    const session = await Session.create({
+      userId: userForToken.id,
+      token: token,
+    });
+
+    response
+      .status(200)
+      .send({token, username: user.username, name: user.name })
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user.id,
+  catch(error)
+  {
+    console.log(error)
   }
-
-  const token = jwt.sign(userForToken, SECRET)
-
-  response
-    .status(200)
-    .send({token, username: user.username, name: user.name })
 })
 
 module.exports = router
--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Blog = require('./blog')
 const User = require('./user')
 const UserBlogs = require('./user_blogs')
+const Session = require('./session')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+Session.belongsTo(User, { foreignKey: 'user_id' })
 
 User.belongsToMany(Blog, { through: UserBlogs, as: 'reading_list' })
 Blog.belongsToMany(User, { through: UserBlogs, as: 'readinglisted_by' })
